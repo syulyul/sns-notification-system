c881a4c8dee81181f2120291068e1aecd88278ae
tjswns <tjswns99@naver.com>
2023-09-14 20:14:41 +0900

    (login)usecase

--- snsapp/app-server/src/main/java/bitcamp/myapp/controller/AuthController.java
+++ snsapp/app-server/src/main/java/bitcamp/myapp/controller/AuthController.java
@@ -1,10 +1,16 @@
 package bitcamp.myapp.controller;
 
+import bitcamp.myapp.vo.Member;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
 @Controller
@@ -14,11 +20,32 @@ public class AuthController {
   {
     System.out.println("AuthController 생성됨!");
   }
+  @PostMapping("login")
+  public String login(
+          String email,
+          String password,
+          String saveEmail,
+          HttpSession session,
+          Model model,
+          HttpServletResponse response) throws Exception {
 
+    if (saveEmail != null) {
+      Cookie cookie = new Cookie("email", email);
+      response.addCookie(cookie);
+    } else {
+      Cookie cookie = new Cookie("email", "no");
+      cookie.setMaxAge(0);
+      response.addCookie(cookie);
+    }
 
-  @GetMapping("form")
-  public void form(@CookieValue(required = false) String email, Model model) {
-    model.addAttribute("email", email);
+    Member loginUser = memberService.get(email, password);
+    if (loginUser == null) {
+      model.addAttribute("refresh", "2;url=form");
+      throw new Exception("회원 정보가 일치하지 않습니다.");
+    }
+
+    session.setAttribute("loginUser", loginUser);
+    return "redirect:/";
   }
 
 
