73c5d1247389d9eb3b0091b6b25583f0b353c4e6
tjswns <tjswns99@naver.com>
2023-09-18 17:26:34 +0900

    test4

--- snsapp/app-server/src/main/java/bitcamp/myapp/controller/AuthController.java
+++ snsapp/app-server/src/main/java/bitcamp/myapp/controller/AuthController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import bitcamp.myapp.service.NcpObjectStorageService;
 import bitcamp.myapp.vo.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/auth")
@@ -24,6 +26,9 @@ public class AuthController {
 
   @Autowired
   MemberService memberService;
+
+  @Autowired
+  NcpObjectStorageService ncpObjectStorageService;
 
   @GetMapping("form")
   public void form(@CookieValue(required = false) String phoneNumber, Model model) {
@@ -56,7 +61,28 @@ public class AuthController {
 //    session.setAttribute("loginUser", loginUser);
     return "redirect:/";
   }
+  @PostMapping("add")
+  public String add(
+          Member member,
+          MultipartFile photofile,
+          Model model) throws Exception {
 
+    try {
+      System.out.println(member);
+      if (photofile.getSize() > 0) {
+        String uploadFileUrl = ncpObjectStorageService.uploadFile(
+                "bitcamp-nc7-bucket-14", "member/", photofile);
+        member.setPhoto(uploadFileUrl);
+      }
+      memberService.add(member);
+      return "redirect:list";
+
+    } catch (Exception e) {
+      model.addAttribute("message", "회원 등록 오류!");
+      model.addAttribute("refresh", "2;url=list");
+      throw e;
+    }
+  }
   @GetMapping("logout")
   public String logout(HttpSession session) throws Exception {
     session.invalidate();
