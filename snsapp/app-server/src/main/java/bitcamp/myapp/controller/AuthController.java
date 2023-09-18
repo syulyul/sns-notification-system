7435daae6d29ef7b5bc8075a492898f037803b66
tjswns <tjswns99@naver.com>
2023-09-18 16:19:31 +0900

    snake표기법 -> camel표기법으로 수정

--- snsapp/app-server/src/main/java/bitcamp/myapp/controller/AuthController.java
+++ snsapp/app-server/src/main/java/bitcamp/myapp/controller/AuthController.java
@@ -4,6 +4,8 @@ import bitcamp.myapp.service.MemberService;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import bitcamp.myapp.vo.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -30,22 +32,28 @@ public class AuthController {
 
   @PostMapping("login")
   public String login(
-      String phone_Number,
+      String phoneNumber,
       String password,
-      String savephone_Number,
+      String savePhoneNumber,
       HttpSession session,
       Model model,
       HttpServletResponse response) throws Exception {
 
-    if (savephone_Number != null) {
-      Cookie cookie = new Cookie("phone_Number", phone_Number);
+    if (savePhoneNumber != null) {
+      Cookie cookie = new Cookie("phone_Number", phoneNumber);
       response.addCookie(cookie);
     } else {
       Cookie cookie = new Cookie("phone_Number", "phone_Number");
       cookie.setMaxAge(0);
       response.addCookie(cookie);
     }
-
+//    Member loginUser = memberService.get(phoneNumber, password);
+//    if (loginUser == null) {
+//      model.addAttribute("refresh", "2;url=form");
+//      throw new Exception("회원 정보가 일치하지 않습니다.");
+//    }
+//
+//    session.setAttribute("loginUser", loginUser);
     return "redirect:/";
   }
 
