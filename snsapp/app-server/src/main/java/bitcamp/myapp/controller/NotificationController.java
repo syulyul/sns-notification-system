787c489da794ba6d58dadb70e7ac5c3ca088342d
pangorithm <phs559800@gmail.com>
2023-09-20 11:02:04 +0900

    feat : header에 링크 기능 추가, 알림 리스트 조회 및 알림 상태 확인 기능
    알림 리스트 조회,
    알림 항목 클릭시 알림 확인상태 변경
    
    notification 기능 구현 #42 알림 리스트 기능 구현 #64

--- snsapp/app-server/src/main/java/bitcamp/myapp/controller/NotificationController.java
+++ snsapp/app-server/src/main/java/bitcamp/myapp/controller/NotificationController.java
@@ -2,38 +2,58 @@ package bitcamp.myapp.controller;
 
 import bitcamp.myapp.service.NotificationService;
 import bitcamp.myapp.vo.Member;
+import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.servlet.http.HttpSession;
-import java.util.ArrayList;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/notification")
 public class NotificationController {
 
-    {
-        System.out.println("NotificationController 생성됨!");
+  @Autowired
+  NotificationService notificationService;
+
+  {
+    System.out.println("NotificationController 생성됨!");
+  }
+
+  @GetMapping("list")
+  public String list(Model model, HttpSession session) throws Exception {
+    model.addAttribute("notiList",
+        notificationService.notiLogList(((Member) session.getAttribute("loginUser")).getNo()));
+    return "notification/list";
+  }
+
+  @GetMapping("updateState")
+  public String updateState(
+      int memberNo,
+      int notiNo,
+      int notiState,
+      @RequestParam String url,
+      HttpSession session) throws Exception {
+
+    Member loginUser = (Member) session.getAttribute("loginUser");
+    if (memberNo == loginUser.getNo()) {
+      notificationService.updateState(notiNo, notiState);
     }
 
-    @Autowired
-    NotificationService notificationService;
+    return "redirect:" + url;
+  }
 
-    @GetMapping("list")
-    public void list(Model model, int memberNo) throws Exception {
-        model.addAttribute("list", notificationService.notiLogList(memberNo));
-    }
+  @PostMapping("updateAllState")
+  public String updateAllState(
+      int notiState,
+      HttpSession session) throws Exception {
 
-    @PostMapping("updateState")
-    public void update(int notiState, int notiNo, HttpSession session) throws Exception {
+    Member loginUser = (Member) session.getAttribute("loginUser");
+    notificationService.updateAllState(loginUser.getNo(), notiState);
 
-        Member loginUser = (Member) session.getAttribute("loginUser");
-
-        notificationService.updateState(notiNo, notiState);
-    }
+    return "redirect:list";
+  }
 
 }
