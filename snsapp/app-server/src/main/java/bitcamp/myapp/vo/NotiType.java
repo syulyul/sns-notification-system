f4c276dbe1bbcf003e4ac999721c6d4f57dd7fc8
pangorithm <phs559800@gmail.com>
2023-09-24 11:48:00 +0900

    feat: 댓글 작성시 알림 추가
    댓글 기능 구현 #109

--- snsapp/app-server/src/main/java/bitcamp/myapp/vo/NotiType.java
+++ snsapp/app-server/src/main/java/bitcamp/myapp/vo/NotiType.java
@@ -5,44 +5,50 @@ import java.util.Objects;
 
 public class NotiType implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-    public static int FOLLOW_TYPE = 1;
-    private int no;
-    private String name;
+  private static final long serialVersionUID = 1L;
+  public static int FOLLOW_TYPE = 1;
+  public static int LIKE_TYPE = 2;
+  public static int COMMENT_TYPE = 1;
+  private int no;
+  private String name;
 
-    @Override
-    public String toString() {
-        return "NotiType{" +
-                "no=" + no +
-                ", name='" + name + '\'' +
-                '}';
-    }
+  @Override
+  public String toString() {
+    return "NotiType{" +
+        "no=" + no +
+        ", name='" + name + '\'' +
+        '}';
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof NotiType notiType)) return false;
-        return no == notiType.no && Objects.equals(name, notiType.name);
-    }
+  @Override
+  public boolean equals(Object o) {
+      if (this == o) {
+          return true;
+      }
+      if (!(o instanceof NotiType notiType)) {
+          return false;
+      }
+    return no == notiType.no && Objects.equals(name, notiType.name);
+  }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(no, name);
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(no, name);
+  }
 
-    public int getNo() {
-        return no;
-    }
+  public int getNo() {
+    return no;
+  }
 
-    public void setNo(int no) {
-        this.no = no;
-    }
+  public void setNo(int no) {
+    this.no = no;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 }
