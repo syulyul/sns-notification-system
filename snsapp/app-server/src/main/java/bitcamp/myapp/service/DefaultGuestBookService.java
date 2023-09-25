0bb6dd16a9c75369d621010f29d5bd5d2b49f7f3
pangorithm <phs559800@gmail.com>
2023-09-25 18:02:26 +0900

    fix: 방명록 페이지네이션 디버그

--- snsapp/app-server/src/main/java/bitcamp/myapp/service/DefaultGuestBookService.java
+++ snsapp/app-server/src/main/java/bitcamp/myapp/service/DefaultGuestBookService.java
@@ -5,105 +5,105 @@ import bitcamp.myapp.vo.GuestBook;
 import bitcamp.myapp.vo.Member;
 import bitcamp.myapp.vo.NotiLog;
 import bitcamp.myapp.vo.NotiType;
+import java.util.List;
+import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.servlet.http.HttpSession;
-import java.util.List;
-
 @Service
 public class DefaultGuestBookService implements GuestBookService {
-    @Autowired
-    GuestBookDao guestBookDao;
 
-    @Autowired
-    HttpSession session;
+  @Autowired
+  GuestBookDao guestBookDao;
 
-    @Autowired
-    NotificationService notificationService;
+  @Autowired
+  HttpSession session;
 
-    {
-        System.out.println("DefaultGuestBookService 생성됨!");
+  @Autowired
+  NotificationService notificationService;
+
+  {
+    System.out.println("DefaultGuestBookService 생성됨!");
+  }
+
+  @Transactional // 이 메서드는 트랜잭션 상태에서 실행하라고 지정
+  @Override
+  public int add(GuestBook guestBook) throws Exception {
+    int count = guestBookDao.insert(guestBook);
+    return count;
+  }
+
+  @Override
+  public List<GuestBook> list(int no, int limit, int page) throws Exception {
+    return guestBookDao.findAll(no, limit, limit * (page - 1));
+  }
+
+  @Override
+  public int getTotalCount(int memNo) throws Exception {
+    return guestBookDao.getTotalCount(memNo);
+  }
+
+  @Override
+  public GuestBook get(int guestBookNo) throws Exception {
+    return guestBookDao.findBy(guestBookNo);
+  }
+
+  @Override
+  public int delete(int guestBookNo) throws Exception {
+    guestBookDao.deleteLikes(guestBookNo);
+    return guestBookDao.delete(guestBookNo);
+  }
+
+  @Override
+  public int increaseLikes(int guestBookNo) throws Exception {
+    return guestBookDao.updateLike(guestBookNo);
+  }
+
+  @Override
+  public int decreaseLikes(int guestBookNo) throws Exception {
+    return guestBookDao.cancelLike(guestBookNo);
+  }
+
+  @Override
+  public int like(Member member, GuestBook guestBook) throws Exception {
+    int result = guestBookDao.insertLike(member.getNo(), guestBook.getNo());
+    if (!session.getAttribute("loginUser").equals(guestBook.getWriter())) {
+      notificationService.add(new NotiLog(
+          guestBook.getWriter().getNo(),
+          NotiType.FOLLOW_TYPE,
+          member.getNick() + "님이 회원의 게시글을 좋아합니다.",
+          "/guestBook/" + guestBook.getMemNo()));
     }
+    return result;
+  }
 
-    @Transactional // 이 메서드는 트랜잭션 상태에서 실행하라고 지정
-    @Override
-    public int add(GuestBook guestBook) throws Exception {
-        int count = guestBookDao.insert(guestBook);
-        return count;
+  @Override
+  public int unlike(Member member, GuestBook guestBook) throws Exception {
+    int result = guestBookDao.deleteLike(member.getNo(), guestBook.getNo());
+    if (!session.getAttribute("loginUser").equals(guestBook.getWriter())) {
+      notificationService.add(new NotiLog(
+          guestBook.getWriter().getNo(),
+          NotiType.FOLLOW_TYPE,
+          member.getNick() + "님이 회원의 게시글 좋아요를 취소 했습니다..",
+          "/guestBook/" + guestBook.getMemNo()));
     }
+    return result;
+  }
 
-    @Override
-    public List<GuestBook> list(int no, int limit, int page) throws Exception {
-        return guestBookDao.findAll(no, limit, limit * (page - 1));
-    }
+  @Override
+  public List<Integer> likelist(int memberNo) throws Exception {
+    return guestBookDao.findLikeByMno(memberNo);
+  }
 
-    @Override
-    public int getTotalCount() throws Exception {
-        return guestBookDao.getTotalCount();
-    }
+  @Override
+  public List<String> guestBooklikelist(int guestBookNo) throws Exception {
+    return guestBookDao.findLikeByBno(guestBookNo);
+  }
 
-    @Override
-    public GuestBook get(int guestBookNo) throws Exception {
-        return guestBookDao.findBy(guestBookNo);
-    }
-
-    @Override
-    public int delete(int guestBookNo) throws Exception {
-        guestBookDao.deleteLikes(guestBookNo);
-        return guestBookDao.delete(guestBookNo);
-    }
-
-    @Override
-    public int increaseLikes(int guestBookNo) throws Exception {
-        return guestBookDao.updateLike(guestBookNo);
-    }
-
-    @Override
-    public int decreaseLikes(int guestBookNo) throws Exception {
-        return guestBookDao.cancelLike(guestBookNo);
-    }
-
-    @Override
-    public int like(Member member, GuestBook guestBook) throws Exception {
-        int result = guestBookDao.insertLike(member.getNo(), guestBook.getNo());
-        if (!session.getAttribute("loginUser").equals(guestBook.getWriter())) {
-            notificationService.add(new NotiLog(
-                    guestBook.getWriter().getNo(),
-                    NotiType.FOLLOW_TYPE,
-                    member.getNick() + "님이 회원의 게시글을 좋아합니다.",
-                    "/guestBook/" + guestBook.getMemNo()));
-        }
-        return result;
-    }
-
-    @Override
-    public int unlike(Member member, GuestBook guestBook) throws Exception {
-        int result = guestBookDao.deleteLike(member.getNo(), guestBook.getNo());
-        if (!session.getAttribute("loginUser").equals(guestBook.getWriter())) {
-            notificationService.add(new NotiLog(
-                    guestBook.getWriter().getNo(),
-                    NotiType.FOLLOW_TYPE,
-                    member.getNick() + "님이 회원의 게시글 좋아요를 취소 했습니다..",
-                    "/guestBook/" + guestBook.getMemNo()));
-        }
-        return result;
-    }
-
-    @Override
-    public List<Integer> likelist(int memberNo) throws Exception {
-        return guestBookDao.findLikeByMno(memberNo);
-    }
-
-    @Override
-    public List<String> guestBooklikelist(int guestBookNo) throws Exception {
-        return guestBookDao.findLikeByBno(guestBookNo);
-    }
-
-    @Override
-    public String getMemberNickByNo(int memberNo) throws Exception {
-        // GuestBookDao를 이용하여 회원 번호로 회원 닉네임을 가져옴
-        return guestBookDao.findNickByMemNo(memberNo);
-    }
+  @Override
+  public String getMemberNickByNo(int memberNo) throws Exception {
+    // GuestBookDao를 이용하여 회원 번호로 회원 닉네임을 가져옴
+    return guestBookDao.findNickByMemNo(memberNo);
+  }
 }
