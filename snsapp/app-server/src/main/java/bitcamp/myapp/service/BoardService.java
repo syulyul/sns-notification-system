20744bc5bc2352e7aa67dcf81724eab63b490e5e
lorajung <lorajung@naver.com>
2023-09-25 13:40:19 +0900

    feat : 제목,내용,작성자명으로 게시글 검색 기능 구현

--- snsapp/app-server/src/main/java/bitcamp/myapp/service/BoardService.java
+++ snsapp/app-server/src/main/java/bitcamp/myapp/service/BoardService.java
@@ -38,4 +38,8 @@ public interface BoardService {
   List<String> boardlikelist(int boardNo) throws Exception;
 
   List<Board> myboardlist(int category, int writerNo, int limit, int page) throws Exception;
+
+  List<Board> searchBoardsList(int category, String keyword, int limit, int page) throws Exception;
+
+  int getSearchBoardsCount(String keyword);
 }
