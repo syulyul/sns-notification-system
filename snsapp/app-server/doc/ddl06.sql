0d33daba5b4d23766408f83c0b11a12df0a0a320
syulyul <syul9800@gmail.com>
2023-09-26 14:12:33 +0900

    docs : 신규 테이블 drop 구문 추가 및 쿼리 위치 변경
    방명록 카테고리 drop 구문 추가 및 방명록 쿼리 위치 변경

--- snsapp/app-server/doc/ddl06.sql
+++ snsapp/app-server/doc/ddl06.sql
@@ -37,6 +37,12 @@ DROP TABLE IF EXISTS sns_today_visit_count RESTRICT;
 -- 게시글 카테고리
 DROP TABLE IF EXISTS sns_board_category RESTRICT;
 
+-- 방명록 카테고리
+DROP TABLE IF EXISTS sns_guestbook RESTRICT;
+
+-- 방명록 카테고리 좋아요
+DROP TABLE IF EXISTS sns_guestbook_like RESTRICT;
+
 
 -- 회원
 CREATE TABLE sns_member (
@@ -102,39 +108,6 @@ ALTER TABLE sns_board
 
 ALTER TABLE sns_board
   MODIFY COLUMN bno INTEGER NOT NULL AUTO_INCREMENT COMMENT '게시글 번호';
-
--- 방명록
-CREATE TABLE sns_guestbook (
-  gbno     INTEGER      NOT NULL COMMENT '번호', -- 번호
-  title    VARCHAR(255) NOT NULL COMMENT '제목', -- 제목
-  content  MEDIUMTEXT   NOT NULL COMMENT '내용', -- 내용
-  likes   INTEGER           NULL COMMENT '좋아요', -- 좋아요
-  created_at DATETIME     NOT NULL DEFAULT now() COMMENT '등록일', -- 등록일
-  mno      INTEGER      NOT NULL COMMENT '회원번호', -- 작성자 번호
-  FOREIGN KEY (mno) REFERENCES sns_member (mno),
-  mpno   INTEGER      NOT NULL COMMENT '회원번호',  -- 당사자 번호
-  FOREIGN KEY (mpno) REFERENCES sns_mypage (mno)
-)
-COMMENT '방명록';
-
--- 방명록
-ALTER TABLE sns_guestbook
-  ADD CONSTRAINT PK_sns_guestbook -- 방명록 기본키
-  PRIMARY KEY (
-  gbno -- 번호
-  );
-
-ALTER TABLE sns_guestbook
-  MODIFY COLUMN gbno INTEGER AUTO_INCREMENT COMMENT '번호';
-
--- 방명록 좋아요
-CREATE TABLE sns_guestbook_like (
-  gbno     INTEGER      NOT NULL COMMENT '번호', -- 번호
-  FOREIGN KEY (gbno) REFERENCES sns_guestbook (gbno),
-  mno      INTEGER      NOT NULL COMMENT '회원번호', -- 작성자 번호
-  FOREIGN KEY (mno) REFERENCES sns_member (mno)
-)
-COMMENT '방명록좋아요';
 
 -- 댓글
 CREATE TABLE sns_board_comment (
@@ -538,3 +511,37 @@ ALTER TABLE sns_today_visit_count
   REFERENCES sns_mypage ( -- 마이페이지
   mno -- 회원번호
   );
+
+
+-- 방명록
+CREATE TABLE sns_guestbook (
+   gbno     INTEGER      NOT NULL COMMENT '번호', -- 번호
+   title    VARCHAR(255) NOT NULL COMMENT '제목', -- 제목
+   content  MEDIUMTEXT   NOT NULL COMMENT '내용', -- 내용
+   likes   INTEGER           NULL COMMENT '좋아요', -- 좋아요
+   created_at DATETIME     NOT NULL DEFAULT now() COMMENT '등록일', -- 등록일
+   mno      INTEGER      NOT NULL COMMENT '회원번호', -- 작성자 번호
+   FOREIGN KEY (mno) REFERENCES sns_member (mno),
+   mpno   INTEGER      NOT NULL COMMENT '회원번호',  -- 당사자 번호
+   FOREIGN KEY (mpno) REFERENCES sns_mypage (mno)
+)
+    COMMENT '방명록';
+
+-- 방명록
+ALTER TABLE sns_guestbook
+    ADD CONSTRAINT PK_sns_guestbook -- 방명록 기본키
+        PRIMARY KEY (
+                     gbno -- 번호
+            );
+
+ALTER TABLE sns_guestbook
+    MODIFY COLUMN gbno INTEGER AUTO_INCREMENT COMMENT '번호';
+
+-- 방명록 좋아요
+CREATE TABLE sns_guestbook_like (
+    gbno     INTEGER      NOT NULL COMMENT '번호', -- 번호
+    FOREIGN KEY (gbno) REFERENCES sns_guestbook (gbno),
+    mno      INTEGER      NOT NULL COMMENT '회원번호', -- 작성자 번호
+    FOREIGN KEY (mno) REFERENCES sns_member (mno)
+)
+    COMMENT '방명록좋아요';
